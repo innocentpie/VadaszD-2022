cc792ba1bb221a8f3e20542d39d2564849026696
Szabó Ármin András <46499638+Lonelymous@users.noreply.github.com>
2022-03-19 22:34:10 +0100

    backup against Gergő

---
+++ VersenyUI/VersenyUI/GameLogic/FieldImplementation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+
+namespace VersenyUI
+{
+    public class FieldImplementation
+    {
+        private Predicate<int[]> condition;
+        private int expectedArraySize;
+
+        public FieldImplementation(Predicate<int[]> condition, int expectedArraySize)
+        {
+            this.condition = condition;
+            this.expectedArraySize = expectedArraySize;
+        }
+
+        public bool MatchesCondition(int[] rolls)
+        {
+            if (rolls.Length != expectedArraySize)
+            {
+                MessageBox.Show("xd");
+                return false;
+            }
+
+            return condition(rolls);
+        }
+    }
+}
--- VersenyUI/VersenyUI/MainWindow.xaml.cs
+++ VersenyUI/VersenyUI/MainWindow.xaml.cs
@@ -20,11 +20,64 @@ namespace VersenyUI
         public MainWindow()
         {
             InitializeComponent();
-            this.Width = System.Convert.ToInt16(System.Math.Round(System.Windows.SystemParameters.FullPrimaryScreenWidth * 0.665));
-            this.Height = System.Convert.ToInt16(System.Math.Round(System.Windows.SystemParameters.FullPrimaryScreenHeight * 0.665));
+            this.Width = Math.Round(SystemParameters.FullPrimaryScreenWidth * 0.665);
+            this.Height = Math.Round(SystemParameters.FullPrimaryScreenHeight * 0.665);
+            Button playButton = new Button();
+            playButton.Content = "Játék";
+            playButton.SetValue(Grid.RowProperty, 1);
+            playButton.SetValue(Grid.ColumnProperty, 1);
+            playButton.Click += new RoutedEventHandler(StartGame);
+            mainGrid.Children.Add(playButton);
+        }
+        private void StartGame(System.Object sender, RoutedEventArgs e)
+        {
+            mainGrid.Children.Clear();
+            Grid gameGrid = new Grid();
+            gameGrid.SetValue(Grid.ColumnSpanProperty, 3);
+            gameGrid.SetValue(Grid.RowSpanProperty, 2);
+            gameGrid.Background = new SolidColorBrush(Colors.White);
+            gameGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
+            gameGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
+            gameGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
+            gameGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
+            gameGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
+            gameGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(10, GridUnitType.Star) });
+            gameGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(15, GridUnitType.Star) });
+            gameGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(60, GridUnitType.Star) });
+            gameGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(15, GridUnitType.Star) });
+
+            Random random = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                Button button = new Button();
+                button.Margin = new Thickness(10);
+                button.MinHeight = 10;
+                button.MinWidth = 10;
+                button.Content = random.Next(1, 7);
+                button.SetValue(Grid.RowProperty, i);
+                button.SetValue(Grid.ColumnProperty, 0);
+                gameGrid.Children.Add(button);
+            }
+
+            DataGrid table = new DataGrid();
+            table.Background = Brushes.White;
+            table.SetValue(Grid.RowProperty, 1);
+            table.SetValue(Grid.RowSpanProperty, 3);
+            table.SetValue(Grid.ColumnProperty, 2);
+            gameGrid.Children.Add(table);
 
 
+            mainGrid.Children.Add(gameGrid);
+            ASDA.xdddddddd(10);
 
+            ASD();
+        }
+
+
+        private void ASD()
+        {
+            MessageBox.Show("XD");
+            
         }
     }
 }
