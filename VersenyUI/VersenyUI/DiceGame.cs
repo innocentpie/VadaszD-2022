17453aef3ea6d5fc1ee619f23918bb2661aa74e7
BlokOfWood <strikerelite@outlook.hu>
2022-04-07 17:59:17 +0200

    Mezők Implementációja

---
+++ VersenyUI/VersenyUI/DiceGame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VersenyUI
+{
+    public class DiceGame
+    {
+        public Field[] GameFields = new Field[] 
+        { 
+            new Field("Szemét", 5),
+            new Field("Pár", 2, FieldPredicates.Pár),
+            new Field("Drill", 3, FieldPredicates.Drill),
+            new Field("Dupla pár", 4, FieldPredicates.DuplaPár),
+            new Field("Kis póker", 4, FieldPredicates.KisPóker),
+            new Field("Full", 5, FieldPredicates.Full),
+            new Field("Kis sor", 5, FieldPredicates.KisSor, 15),
+            new Field("Nagy sor", 5, FieldPredicates.NagySor, 20),
+            new Field("Nagy póker", 5, FieldPredicates.NagyPóker, 50)
+        };
+    }
+}
--- VersenyUI/VersenyUI/GameLogic/FieldImplementation.cs
+++ VersenyUI/VersenyUI/GameLogic/FieldImplementation.cs
@@ -1,28 +1,117 @@
 ﻿using System;
+using System.Linq;
 using System.Windows;
+using System.Collections.Generic;
 
 namespace VersenyUI
-{
-    public class FieldImplementation
+{    
+    public class Field
     {
-        private Predicate<int[]> condition;
-        private int expectedArraySize;
+        public string Name { get; protected set; } //The name of the field.
+        protected int NumberOfDice; //Expected number of dice in this field.
+        protected int[] DiceValues { get; set; } //The dice that were given to this field.
 
-        public FieldImplementation(Predicate<int[]> condition, int expectedArraySize)
-        {
-            this.condition = condition;
-            this.expectedArraySize = expectedArraySize;
-        }
+        public int Points => //The number of points this field is worth.
+            overriddenPointValue == null 
+            ? DiceValues.Sum() 
+            : (int)overriddenPointValue;
 
-        public bool MatchesCondition(int[] rolls)
+        
+        int? overriddenPointValue = null; //Contains the point value that the Field was given.
+        //If left null, the point value will just be the sum of all given dice values.
+
+        protected Predicate<int[]> CheckConditionPredicate;
+        
+        protected bool CheckCondition(int[] dice)
         {
-            if (rolls.Length != expectedArraySize)
-            {
-                MessageBox.Show("xd");
+            if (dice.Length != NumberOfDice)
                 return false;
-            }
 
-            return condition(rolls);
+            if (CheckConditionPredicate == null)
+                return true;
+
+            return CheckConditionPredicate.Invoke(dice);
         }
+
+        public bool AssignValues(int[] _dice)
+        {
+            if(!CheckCondition(_dice))
+                return false;
+
+            DiceValues = _dice;
+            return true;
+        }
+
+        public Field(string _name, int _numberOfDice, Predicate<int[]> _checkConditionPredicate = null, int? _overriddenPointValue = null)
+        {
+            Name = _name;
+            CheckConditionPredicate = _checkConditionPredicate;
+            NumberOfDice = _numberOfDice;
+            overriddenPointValue = _overriddenPointValue;
+        }
+    }
+    public static class FieldPredicates
+    {
+        public static Predicate<int[]> Pár =
+            dice => dice.Count(x => x == dice[0]) == 2;
+
+        public static Predicate<int[]> Drill =
+            dice => dice.Count(x => x == dice[0]) == 3;
+
+        public static Predicate<int[]> DuplaPár =
+            dice =>
+            {
+                List<int> sorted = dice.ToList();
+                sorted.Sort();
+
+                return
+                    sorted.Count(x => sorted[0] == x) == 2 &&
+                    sorted.Count(x => sorted[2] == x) == 2;
+            };
+        
+        public static Predicate<int[]> KisPóker =
+            dice => dice.Count(x => x == dice[0]) == 4;
+
+        public static Predicate<int[]> Full =
+            dice =>
+            {
+                List<int> sorted = dice.ToList();
+                sorted.Sort();
+
+                return
+                    sorted.Count(x => sorted[0] == x) == 2 &&
+                    sorted.Count(x => sorted[2] == x) == 3;
+            };
+
+        public static Predicate<int[]> KisSor =
+            dice =>
+            {
+                List<int> sorted = dice.ToList();
+                sorted.Sort();
+
+                for (int i = 1; i < 6; i++)
+                {
+                    if (sorted[i - 1] != i)
+                        return false;
+                }
+                return true;
+            };
+
+        public static Predicate<int[]> NagySor =
+            dice =>
+            {
+                List<int> sorted = dice.ToList();
+                sorted.Sort();
+
+                for (int i = 2; i < 7; i++)
+                {
+                    if (sorted[i - 2] != i)
+                        return false;
+                }
+                return true;
+            };
+
+        public static Predicate<int[]> NagyPóker =
+            dice => dice.Count(x => x == dice[0]) == 5;
     }
 }
